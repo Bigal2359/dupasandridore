b525f0297a0475f15db930d8495a723af3dbc465
Bigal2359 <alexridore@gmail.com>
2016-05-22 10:13:08 -0400

    reimplemented initial menu

--- js/script.js
+++ js/script.js
@@ -56,7 +56,6 @@ $('input#mc-embedded-subscribe').click(function(){
 
 $('.btn-nav-menu').click(function(){
     $('body').toggleClass('menu-active');
-    $('.nav-menu').fadeToggle();
 });
 
 setTimeout(function() {
@@ -104,9 +103,9 @@ Raphael(function(){
         third.animate({path: 'M0 21L50 21',stroke: '#000'},200,'ease-in-out');
         n--;
       }else{
-        first.animate({path: 'M0 1L50 31',stroke: '#000'},200,'ease-in-out');
+        first.animate({path: 'M0 1L50 31',stroke: '#FFF'},200,'ease-in-out');
         second.animate({opacity:0},50,'ease-in-out');
-        third.animate({path: 'M0 31L50 1',stroke: '#000'},200,'ease-in-out');
+        third.animate({path: 'M0 31L50 1',stroke: '#FFF'},200,'ease-in-out');
         n++;
       }
     });
